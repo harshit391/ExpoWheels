8cf9a9b02579d891707fdc15805428bb0c844e96
Harshit Singla <harshitsingla1761@gmail.com>
2024-11-23 12:24:17 +0530

    Tailwind Installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-    return <div>Singla Making The Site</div>;
+    return (
+        <div
+            style={{ textUnderlineOffset: "1rem", fontFamily: "Poppins" }}
+            className="flex p-4 min-h-screen justify-center font-semibold text-5xl items-center underline bg-gradient-to-bl from-indigo-100 to-indigo-200"
+        >
+            Singla Making The Site
+        </div>
+    );
 };
 
 export default App;
