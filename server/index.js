3d652aa704a4a23b84123d5f2000d8563711f388
Harshit Singla <harshitsingla1761@gmail.com>
2024-11-27 20:52:38 +0530

    [Backend] - Intialized

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send({message: "Hello World!"});
+});
+
 app.listen(1009, () => {
     console.log('Server is running on port http://localhost:1009');
 })
