0458b1844dd593028ad100c1715145bd310fc2c9
Harshit Singla <harshitsingla1761@gmail.com>
2024-11-27 21:09:53 +0530

    [Backend] | AWS Friendly Ports

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
+// app.use("/", express.static("./../frontend/dist"))
+
 app.get("/", (req, res) => {
     res.send({message: "Hello World!"});
 });
 
-app.listen(1009, () => {
-    console.log('Server is running on port http://localhost:1009');
+app.listen(8081, () => {
+    console.log('Server is running on port http://localhost:8081');
 })
